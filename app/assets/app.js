bacc7eaff1923a0e41f27c15f3f2293539bd6e95
frankwallen <frank.wallen@gmail.com>
2021-01-31 20:03:30 -0800

    Set up Bulma

--- app/assets/app.js
+++ app/assets/app.js
@@ -5,8 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import 'bulma/bulma.sass';
+
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// import './styles/app.css';
+
 
 import './bootstrap';
 
